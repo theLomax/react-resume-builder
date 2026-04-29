bc0251f644d863d29627622e758481279dacfed9
theLomax <thelomax@gmail.com>
2026-04-29 09:30:09 -0500

    Create resume.ts and profile interface
    - create resume.ts
    - build ResumeProfile interface

---
+++ src/types/resume.ts
@@ -0,0 +1,12 @@
+export interface ResumeProfile {
+	firstName: string
+	lastName: string
+	title?: string
+	email: string
+	phone?: string
+	linkedin?: string
+	city?: string
+	state?: string
+	zipCode?: string
+}
+
