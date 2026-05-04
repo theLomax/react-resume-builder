19e5a9efac21801fb149673731c0d1caa6055e7f
James <thelomax@gmail.com>
2026-05-03 19:54:08 -0500

    CHORE: add svgr, router, TanStack Query; update config and deps

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { ResumePage } from './components/ResumePage'
+import { SkillsTestPage } from './components/SkillsTestPage'
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ResumePage />} />
+        <Route path="/test/skills" element={<SkillsTestPage />} />
       </Routes>
     </BrowserRouter>
   )
